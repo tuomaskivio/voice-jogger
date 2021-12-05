73a0ecf4f559592266df1d6df2b7fe96e87c1df3
spomha <omar.hassan@tuni.fi>
2021-12-05 18:18:46 +0200

    Add ROS component

--- src/server/main.py
+++ src/server/main.py
@@ -1,3 +1,4 @@
+"""Main server code for handling Android->Speech2text->ROS. Use Ctrl-C to stop script"""
 import queue
 
 from commandCreator import CommandCreator
@@ -5,22 +6,28 @@ from udp_handler import UDPReceiver
 from model_handler import Recognizer
 
 # Configs:
-# Rate = 16000, CHUNK = 1280
-# Rate = 8000, CHUNK = 640
+# if Rate = 16000, then Chunk = 1280
+# if Rate = 8000, then Chunk = 640 (8kHz is not supported with speech model. Can be added later with upsampling before passing to recognizer)
 
-RATE = 16000 # 16000 is good for RawInputStream. Higher takes more wrong words. (32000 is the best, 48000 works pretty good)
+RATE = 16000
 CHUNK = 1280
 PORT = 50005
+ROS_ENABLED = False
 
-q = queue.Queue()
+if ROS_ENABLED:
+    # ROS
+    import rospy
+    from std_msgs.msg import String
+
+    rospy.init_node('text_command_transmitter')
+    pub = rospy.Publisher("/text_commands", String, queue_size=10) # queue_size gives time for subscriber to process data it gets
 
 # UDP Receiver (Handles Android App comm.)
+q = queue.Queue()
 udp = UDPReceiver(q, CHUNK, "0.0.0.0", PORT)
 
 # Speech Recognizer (Handles speech to text)
 rec = Recognizer('model', RATE)
-cmd = None
-start_robot = False
 
 # Command Creator (Handles words to command logic)
 commandCreator = CommandCreator()
@@ -28,15 +35,17 @@ commandCreator = CommandCreator()
 # Start udp thread
 udp.start()
 
+cmd = None
+start_robot = False
 try:
 
     while True:
         data = q.get()
-        # mode: step mode. Faster response time, because in the direction and distance mode distance needs to be recognized.
         
         words = rec.speech_to_text(data)
         if len(words) > 1:
             print(words)
+            # mode: step mode. Faster response time, because in the direction and distance mode, distance needs to be recognized.
             cmd = commandCreator.getCommand(words)
 
         if cmd is not None:
@@ -44,6 +53,11 @@ try:
             if cmd[0] == 'START':
                 start_robot = True
                 print('Starting with command: ', cmd)
+                print(f'Sending configuration to ROS. mode: {commandCreator.mode} step_size: {commandCreator.step_size}')
+                if ROS_ENABLED:
+                    # Send robot configuration to ROS.
+                    pub.publish('MODE ' + commandCreator.mode)
+                    pub.publish('STEP SIZE ' + commandCreator.step_size)
 
             elif cmd[0] == 'STOP':
                 start_robot = False
@@ -52,7 +66,9 @@ try:
             # cmds are published after the robot is started
             if start_robot and cmd is not None:
                 cmdString = ' '.join(map(str, cmd))
-                print(cmdString)
+                print('Sending Command to ROS: ', cmdString)
+                if ROS_ENABLED:
+                    pub.publish(cmdString)
             cmd = None
 
 except Exception as e:
