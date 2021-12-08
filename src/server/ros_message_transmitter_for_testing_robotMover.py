dbe1681b745377e91adab3103f59bf635fd90627
Mikko Kulju <kulju.mikko.e@gmail.com>
2021-12-08 20:20:32 +0200

    Added scripts for testing system with microphone and sending test text commands.

---
+++ src/server/ros_message_transmitter_for_testing_robotMover.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import rospy
+from std_msgs.msg import String
+
+if __name__== '__main__':
+	rospy.init_node('text_command_transmitter')
+
+	pub = rospy.Publisher("/text_commands", String, queue_size=10) # queue_size gives time for subscriber to process data it gets
+	
+	print("Give capitalized commands:")
+	print("Stepping mode: ")
+	print("MOVE UP, DOWN, LEFT, RIGHT, FORWARD, BACKWARD")
+	print("")
+	print("Direction and distance mode:")
+	print("MOVE UP 10, MOVE DOWN 20...")
+	print("")
+	print("Change step size:")
+	print("STEP SIZE LOW/MEDIUM/HIGH")
+	print("")
+	print("Change mode:")
+	print("MODE STEP/DISTANCE")
+	print("")
+	
+
+	while not rospy.is_shutdown():
+		inputCommand = input()
+		msg = String()
+		msg.data = inputCommand
+		pub.publish(msg)
+
+	rospy.loginfo("Node was stopped")
