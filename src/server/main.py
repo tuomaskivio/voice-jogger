312b570f6f0fe36ff540a6e77c167ae9470d9011
Tuomas Kiviö <tuomaskivio@gmail.com>
2023-01-28 14:47:06 +0200

    Fix main when ros enabled false

--- src/server/main.py
+++ src/server/main.py
@@ -28,103 +28,103 @@ class Server:
             self.pub = rospy.Publisher("/text_commands", String, queue_size=10) # queue_size gives time for subscriber to process data it gets
             self.pub_priority = rospy.Publisher("/text_commands_priority", String, queue_size=10) # queue_size gives time for subscriber to process data it gets
 
-            # UDP Receiver (Handles Android App comm.)
-            q = queue.Queue()
-            self.udp = UDPReceiver(q, CHUNK, "0.0.0.0", PORT)
+        # UDP Receiver (Handles Android App comm.)
+        q = queue.Queue()
+        self.udp = UDPReceiver(q, CHUNK, "0.0.0.0", PORT)
 
-            # Speech Recognizer (Handles speech to text)
-            rec = Recognizer('model', RATE, DEBUG)
+        # Speech Recognizer (Handles speech to text)
+        rec = Recognizer('model', RATE, DEBUG)
 
-            # Command Creator (Handles words to command logic)
-            self.commandCreator = CommandCreator(debug_enabled=DEBUG)
+        # Command Creator (Handles words to command logic)
+        self.commandCreator = CommandCreator(debug_enabled=DEBUG)
 
-            # Start udp thread
-            self.udp.start()
+        # Start udp thread
+        self.udp.start()
 
-            cmd = None
-            self.start_robot = False
-            self.is_chain_going = False
-            self.previous_and = False
-            self.prev_cmd = None
-            try:
+        cmd = None
+        self.start_robot = False
+        self.is_chain_going = False
+        self.previous_and = False
+        self.prev_cmd = None
+        try:
 
-                while True:
-                    # We need this to make this process shutdown when rospy is being used
-                    if ROS_ENABLED and rospy.is_shutdown():
-                        break
-                    if q.empty():
-                        continue
-                    data = q.get()
+            while True:
+                # We need this to make this process shutdown when rospy is being used
+                if ROS_ENABLED and rospy.is_shutdown():
+                    break
+                if q.empty():
+                    continue
+                data = q.get()
 
-                    words = rec.speech_to_text(data)
+                words = rec.speech_to_text(data)
 
-                    chained_command = False
+                chained_command = False
 
-                    if words != None:
-                        # mode: step mode. Faster response time, because in the direction and distance mode, distance needs to be recognized.
-                        self.commandCreator.original_words = words
-                        cmd = self.commandCreator.getCommand(True)
+                if words != None:
+                    # mode: step mode. Faster response time, because in the direction and distance mode, distance needs to be recognized.
+                    self.commandCreator.original_words = words
+                    cmd = self.commandCreator.getCommand(True)
 
-                    if cmd is not None:
-                        #check if there will be another command
-                        next_words, is_and, is_end_of_and = self.commandCreator.check_if_chained(words)
-                        if next_words is not None and len(next_words) != 0:
-                            chained_command = True
-                            try:
-                                cmd.append(is_and)
-                                cmd.append(is_end_of_and)
-                                self.previous_and = is_and
-                            except:
-                                pass
+                if cmd is not None:
+                    #check if there will be another command
+                    next_words, is_and, is_end_of_and = self.commandCreator.check_if_chained(words)
+                    if next_words is not None and len(next_words) != 0:
+                        chained_command = True
+                        try:
+                            cmd.append(is_and)
+                            cmd.append(is_end_of_and)
+                            self.previous_and = is_and
+                        except:
+                            pass
 
-                        #start_robot means start sending commands
-                        if cmd[0] == 'START':
-                            self.start_robot = True
-                            print('Starting with command: ', cmd)
-                            print(f'Sending configuration to ROS. mode: {self.commandCreator.mode} step_size: {self.commandCreator.step_size}')
-                            if ROS_ENABLED:
-                                # Send robot configuration to ROS.
-                                self.pub.publish('MODE ' + self.commandCreator.mode)
-                                self.pub.publish('STEP SIZE ' + self.commandCreator.step_size)
+                    #start_robot means start sending commands
+                    if cmd[0] == 'START':
+                        self.start_robot = True
+                        print('Starting with command: ', cmd)
+                        print(f'Sending configuration to ROS. mode: {self.commandCreator.mode} step_size: {self.commandCreator.step_size}')
+                        if ROS_ENABLED:
+                            # Send robot configuration to ROS.
+                            self.pub.publish('MODE ' + self.commandCreator.mode)
+                            self.pub.publish('STEP SIZE ' + self.commandCreator.step_size)
 
-                        elif cmd[0] == 'STOP':
-                            print('Sending Command to ROS: STOP')
-                            if ROS_ENABLED:
-                                self.pub_priority.publish('STOP')
-                            self.start_robot = False
-                            print('Stopping with command: ', cmd)
+                    elif cmd[0] == 'STOP':
+                        print('Sending Command to ROS: STOP')
+                        if ROS_ENABLED:
+                            self.pub_priority.publish('STOP')
+                        self.start_robot = False
+                        print('Stopping with command: ', cmd)
 
-                        elif cmd[0] =='AGAIN' and self.start_robot:
-                            cmdString = ' '.join(map(str, self.prev_cmd))
-                            print('Sending AGAIN Command to ROS: ', cmdString)
+                    elif cmd[0] =='AGAIN' and self.start_robot:
+                        cmdString = ' '.join(map(str, self.prev_cmd))
+                        print('Sending AGAIN Command to ROS: ', cmdString)
+                        if ROS_ENABLED:
+                            self.pub.publish(cmdString)
+
+                    # cmds are published after the robot is started
+                    if self.start_robot and cmd is not None:                
+                        if cmd[0] !='AGAIN':
+                            self.prev_cmd = cmd
+                            cmdString = ' '.join(map(str, cmd))
+                            print('Sending Command to ROS: ', cmdString)
                             if ROS_ENABLED:
                                 self.pub.publish(cmdString)
+                            if chained_command:
+                                # A new command execution and chaining check
+                                self.__check_chained__(next_words)
+                elif words != None and ROS_ENABLED:
+                    self.pub.publish('INVALID')
+                cmd = None
 
-                        # cmds are published after the robot is started
-                        if self.start_robot and cmd is not None:                
-                            if cmd[0] !='AGAIN':
-                                self.prev_cmd = cmd
-                                cmdString = ' '.join(map(str, cmd))
-                                print('Sending Command to ROS: ', cmdString)
-                                if ROS_ENABLED:
-                                    self.pub.publish(cmdString)
-                                if chained_command:
-                                    # A new command execution and chaining check
-                                    self.__check_chained__(next_words)
-                    elif words != None:
-                        self.pub.publish('INVALID')
-                    cmd = None
-
-            except Exception as e:
-                print('Exception', e)
-            finally:
-                print('Shutting down...')
-                print('Closing UDP thread')
-                self.udp.close_thread = True
-                self.udp.join()
-                print('Clearing queue')
-                while not q.empty():
-                    q.get()
+        except Exception as e:
+            print('Exception', e)
+        finally:
+            print('Shutting down...')
+            print('Closing UDP thread')
+            self.udp.close_thread = True
+            self.udp.join()
+            print('Clearing queue')
+            while not q.empty():
+                q.get()
 
     def __check_chained__(self, words):
         chained_command = False
